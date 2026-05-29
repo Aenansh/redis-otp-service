7c60bfabf3736f84b791261b0901f3a08ea91312
aenansh <231030317@juitsolan.in>
2026-05-30 00:30:08 +0530

    set attempt keys

--- server/controllers/otp.controllers.js
+++ server/controllers/otp.controllers.js
@@ -1,4 +1,9 @@
-import { checkBlocked, validateIdentifier } from "../utils/otp.util.js";
+import {
+  attemptsKey,
+  checkBlocked,
+  setAndIncrAttempts,
+  validateIdentifier,
+} from "../utils/otp.util.js";
 
 const sendOtp = async (req, res) => {
   try {
@@ -17,13 +22,23 @@ const sendOtp = async (req, res) => {
         .json({ message: `Invalid ${method} provided.`, success: false });
     }
 
-    if (checkBlocked(identifier))
+    if (await checkBlocked(identifier)) {
+      return res.status(403).json({
+        message: "You are temporarily banned due to multiple requests.",
+        success: false,
+      });
+    }
+
+    const attempts = await setAndIncrAttempts(identifier);
+
+    if (!attempts) {
       return res
         .status(403)
         .json({
-          message: "You are temporarily banned due to multiple requests.",
+          message: "Some problem occured or your allowed attempts exhausted.",
           success: false,
         });
+    }
   } catch (error) {
     console.log("Error in sending OTP");
     res.status(500).json({ message: "Internal Server Error.", success: false });
--- server/utils/otp.util.js
+++ server/utils/otp.util.js
@@ -1,6 +1,8 @@
-import redis from "../config/redis.config.js"
+import redis from "../config/redis.config.js";
 
-export function validateIdentifier(identifier, method) {
+const maxAttemptsAmount = process.env.ATTEMPTS_ALLOWED || 5;
+
+function validateIdentifier(identifier, method) {
   if (typeof identifier !== "string" || identifier.trim()) return false;
   if (method === "email") {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -12,12 +14,48 @@ export function validateIdentifier(identifier, method) {
   return false;
 }
 
-export function otpBlockedKey(identifier) {
+function otpBlockedKey(identifier) {
   return `otp:${identifier}:blocked`;
 }
 
-export function checkBlocked(identifier) {
-  const key = otpBlockedKey(identifier);
-  const isBlocked = await redis.exists(key);
-  return isBlocked;
+function attemptsKey(identifier) {
+  return `otp:${identifier}:attempts`;
 }
+
+async function checkBlocked(identifier) {
+  try {
+    const key = otpBlockedKey(identifier);
+    const isBlocked = await redis.exists(key);
+    return isBlocked;
+  } catch (error) {
+    console.log("Error in checking blocked", error);
+    return false;
+  }
+}
+
+async function setAndIncrAttempts(identifier) {
+  try {
+    const attemptKey = attemptsKey(identifier);
+
+    const attempts = await redis.incr(attemptKey);
+
+    if (attempts === 1) {
+      await redis.expire(attemptKey, 3600);
+    } else if (attempts > maxAttemptsAmount) {
+      await redis.set(otpBlockedKey(identifier), true);
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.log("Error in setting attempts", error);
+    return false;
+  }
+}
+
+export {
+  validateIdentifier,
+  otpBlockedKey,
+  attemptsKey,
+  checkBlocked,
+  setAndIncrAttempts,
+};
